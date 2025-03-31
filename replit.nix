45a45399d499b1b4441cd24932217dac4bc0f061
giacomobolon <39577120-giacomobolon@users.noreply.replit.com>
2025-03-31 13:39:42 +0000

    Restored to 'ac9d178cee47ee0e920004937a5de20e9ad96aa2'
    Replit-Restored-To: ac9d178cee47ee0e920004937a5de20e9ad96aa2

--- replit.nix
+++ replit.nix
@@ -1,21 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.pango
-    pkgs.harfbuzz
-    pkgs.glib
-    pkgs.ghostscript
-    pkgs.fontconfig
-    pkgs.zlib
-    pkgs.tk
-    pkgs.tcl
-    pkgs.openjpeg
-    pkgs.libxcrypt
-    pkgs.libwebp
-    pkgs.libtiff
-    pkgs.libjpeg
-    pkgs.libimagequant
-    pkgs.lcms2
-    pkgs.freetype
     pkgs.glibcLocales
   ];
 }
